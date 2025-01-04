f75fbcca90b25400a61f940958badadaf6c4a77f
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2025-01-04 23:36:53 +0600

    Update le21.cpp

--- cpp_files/le21.cpp
+++ cpp_files/le21.cpp
@@ -1,17 +1,16 @@
-#include &lt;iostream&gt;
-#include &lt;string&gt;
+#include <iostream>
+#include <string>
 using namespace std;
 int main ()
 {
 int i;
-cout &lt;&lt; &quot;Please enter an integer value: &quot;;
-cin &gt;&gt; i;
-cout &lt;&lt; &quot;The value you entered is &quot; &lt;&lt; i;
-cout &lt;&lt; &quot; and its double is &quot; &lt;&lt; i*2 &lt;&lt;
-&quot;.\n&quot;;
+cout << "Please enter an integer value: ";
+cin >> i;
+cout << "The value you entered is " << i;
+cout <<" and its double is " << i*2 << "\n ";
 string mystr;
-cout &lt;&lt; &quot;What&#39;s your name? &quot;;
+cout <<"What's your name?";
 getline (cin, mystr);
-cout &lt;&lt; &quot;Hello &quot; &lt;&lt; mystr &lt;&lt; &quot;.\n&quot;;
+cout << "Hello" << mystr << "\n";
 return 0;
 }
