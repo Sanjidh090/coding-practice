f71cc03c7a781ee1c3779399f3cfb3f193e04999
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2025-01-04 23:30:58 +0600

    Create le02.cpp

---
+++ cpp_files/le02.cpp
@@ -0,0 +1,11 @@
+//my first strung
+#include <iostream>
+#include <string>
+using namespace std;
+ int main() {
+string z= "You can and you will In sha ALLAH";
+cout<< z<< endl;
+ z = " Hope for the better day";
+cout  << z << endl;
+return 0;
+ }
