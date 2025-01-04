25cc3294d001078f9686adbd610e029e822b0761
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2025-01-04 23:38:19 +0600

    Update le21.cpp

--- cpp_files/le21.cpp
+++ cpp_files/le21.cpp
@@ -7,10 +7,10 @@ int i;
 cout << "Please enter an integer value: ";
 cin >> i;
 cout << "The value you entered is " << i;
-cout <<" and its double is " << i*2 << "\n ";
+cout <<" and its double is " << i*2 ;
 string mystr;
 cout <<"What's your name?";
 getline (cin, mystr);
-cout << "Hello" << mystr << "\n";
+cout << "Hello" << mystr;
 return 0;
 }
